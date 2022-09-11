c0850f7ba66849f922f93b3566892738f97dd281
Amr Zayet <amrz.edu@gmail.com>
2022-09-12 01:25:28 +0300

    Implemented form configuration for nested attributes of portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,14 +9,14 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
-        # format.html { redirect_to @portfolio_item, notice: "Your portfolio item is now live." } # We decided not to move the page to the created porfolio this time.
         format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
       else
         format.html { render :new }
