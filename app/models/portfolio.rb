a5bfaf96422c2a0d6dcbce41622506dd5c2905b5
Amr Zayet <amrz.edu@gmail.com>
2022-09-11 21:03:32 +0300

    Implemented concern for generating image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -11,7 +13,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600X400"
-    self.thumb_image ||= "https://via.placeholder.com/350X200"
+    self.main_image ||= Placeholder.image_generator(600, 400)
+    self.thumb_image ||= Placeholder.image_generator(350, 200)
   end
 end
